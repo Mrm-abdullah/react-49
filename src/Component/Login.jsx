c352b79b486442aa87fea5f36f32feb04eee741f
mrmabdullah <mohammodraselmia128@gmail.com>
2024-04-13 16:42:34 +0600

    login with email

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import app from "../firebase/firebase.init";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
 
 
 const Login = () => {
@@ -69,8 +69,81 @@ const Login = () => {
                 console.log(error);
             });
     }
+
+
+
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [show, setShow] = useState(false)
+    const loginForm = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        setError('')
+        setSuccess('')
+        if (password.length < 6) {
+            setError('Password should be at least 6 characters (auth/weak-password).')
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setError('please provide 1 upper case')
+            return;
+        }
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                setUser(user)
+                setSuccess('User Successfully add')
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                setError(errorMessage)
+            });
+    }
+    const showHide = () => {
+        setShow(!show)
+    }
     return (
         <div className="text-center">
+            <div className="hero bg-base-200 my-10">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card shrink-0 w-full shadow-2xl bg-base-100">
+                        <form onSubmit={loginForm} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" name="email" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type={show ? 'text' : 'password'} placeholder="password" name="password" className="input input-bordered" required /> <span className="btn" onClick={showHide}>Show</span>
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                {
+                    error && <>{error}</>
+                }
+
+                {
+                    success && <>{success}</>
+                }
+            </div>
             <button className="btn" onClick={handleGoogleSignIn}>google</button>
             <button className="btn" onClick={handleGithubSignIn}>github</button>
             {
--- src/Component/Reguster.jsx
+++ src/Component/Reguster.jsx
@@ -1,32 +1,52 @@
 
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.init";
-const auth = getAuth(app);
-const registerForm = e => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log(email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        })
-        .catch((error) => {
-            // const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage);
-            // ..
-        });
-}
+import { useState } from "react";
+
+
 
 const Reguster = () => {
+    const auth = getAuth(app);
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [show, setShow] = useState(false)
+    const registerForm = e => {
+        e.preventDefault();
 
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
 
+        setError('')
+        setSuccess('')
+        if (password.length<6) {
+            setError('Password should be at least 6 characters (auth/weak-password).')
+            return;
+        }
+        else if (!/[A-Z]/.test(password)){
+            setError('please provide 1 upper case')
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                setSuccess('User Successfully add')
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                setError(errorMessage)
+            });
+    }
+    const showHide = () => {
+        setShow(!show)
+    }
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card shrink-0 w-full shadow-2xl bg-base-100">
                     <form onSubmit={registerForm} className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -38,7 +58,7 @@ const Reguster = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                            <input type={show ? 'text' : 'password'} placeholder="password" name="password" className="input input-bordered" required /> <span className="btn" onClick={showHide}>Show</span>
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -49,6 +69,13 @@ const Reguster = () => {
                     </form>
                 </div>
             </div>
+            {
+                error && <>{error}</>
+            }
+            
+            {
+                success && <>{success}</>
+            }
         </div>
     );
 };
